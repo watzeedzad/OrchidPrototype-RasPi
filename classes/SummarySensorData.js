645e279c3cff5b99a4ea59dc10087436b62f39a6
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-08-19 20:33:13 +0700

    ...

--- classes/SummarySensorData.js
+++ classes/SummarySensorData.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const tempGreenHouseData = mongoose.model("temp_greenhouse_data");
 const tempProjectData = mongoose.model("temp_project_data");
-const knowController = mongoose.model("know_controller");
+const request = require("request");
+const simpleStats = require("simple-statistics");
 
 let tempGreenHouseDataResult;
 let tempProjectDataResult;
@@ -14,13 +15,85 @@ export default class SummarySernsorData {
     async operation(req, res) {
         await getTempGreenHouseData(macAddressGlobal);
         await getTempProjectData(macAddressGlobal);
-        let rawTempGreenHouseData = {};
-        let rawTempProjectData = {};
+        let rawTempGreenHouseData = [];
+        let rawTempProjectData = [];
+        let currentRawTempGreenHouseIndex = 0;
+        let currentRawTempProjetIndex = 0;
         for (let index = 0; index < tempGreenHouseDataResult.length; index++) {
-            
+            let unAvgData;
+            if (rawTempGreenHouseData.length == 0) {
+                unAvgData = {
+                    farmId: tempGreenHouseDataResult[index].farmId,
+                    greenHouseId: tempGreenHouseDataResult[index].greenHouseId,
+                    ip: tempGreenHouseDataResult[index].ip,
+                    piMacAddress: tempGreenHouseDataResult[index].piMacAddress,
+                    temperature: [tempGreenHouseDataResult[index].temperature],
+                    humidity: [tempGreenHouseDataResult[index].humidity],
+                    soilMoisture: [tempGreenHouseDataResult[index].soilMoisture],
+                    ambientLight: [tempGreenHouseDataResult[index].ambientLight]
+                }
+                rawTempGreenHouseData.push(unAvgData);
+            } else if (rawTempGreenHouseData[currentRawTempGreenHouseIndex].greenHouseId == tempGreenHouseDataResult[index].greenHouseId) {
+                let temp = rawTempGreenHouseData[currentRawTempGreenHouseIndex];
+                temp.temperature.push(tempGreenHouseDataResult[index].temperature);
+                temp.humidity.push(tempGreenHouseDataResult[index].humidity);
+                temp.soilMoisture.push(tempGreenHouseDataResult[index].soilMoisture);
+                temp.ambientLight.push(tempGreenHouseDataResult[index].ambientLight);
+                rawTempGreenHouseData[currentRawTempGreenHouseIndex] = temp;
+            } else {
+                unAvgData = {
+                    farmId: tempGreenHouseDataResult[index].farmId,
+                    greenHouseId: tempGreenHouseDataResult[index].greenHouseId,
+                    ip: tempGreenHouseDataResult[index].ip,
+                    piMacAddress: tempGreenHouseDataResult[index].piMacAddress,
+                    temperature: [tempGreenHouseDataResult[index].temperature],
+                    humidity: [tempGreenHouseDataResult[index].humidity],
+                    soilMoisture: [tempGreenHouseDataResult[index].soilMoisture],
+                    ambientLight: [tempGreenHouseDataResult[index].ambientLight]
+                }
+                rawTempGreenHouseData.push(unAvgData);
+                currentRawTempGreenHouseIndex++;
+            }
         }
         for (let index = 0; index < tempProjectDataResult.length; index++) {
-
+            let unAvgData;
+            if (rawTempProjectData.length == 0) {
+                unAvgData = {
+                    farmId: tempProjectDataResult[index].farmId,
+                    projectId: tempProjectDataResult[index].projectId,
+                    ip: tempProjectDataResult[index].ip,
+                    piMacAddress: tempProjectDataResult[index].piMacAddress,
+                    soilFertility: [tempProjectDataResult[index].soilFertility]
+                }
+                rawTempProjectData.push(unAvgData);
+            } else if (rawTempProjectData[currentRawTempProjetIndex].projectId == tempProjectDataResult[index].projectId) {
+                let temp = rawTempProjectData[currentRawTempProjetIndex];
+                temp.soilFertility.push(tempProjectDataResult[index].soilFertility);
+                rawTempProjectData[currentRawTempProjetIndex] = temp;
+            } else {
+                unAvgData = {
+                    farmId: tempProjectDataResult[index].farmId,
+                    projectId: tempProjectDataResult[index].projectId,
+                    ip: tempProjectDataResult[index].ip,
+                    piMacAddress: tempProjectDataResult[index].piMacAddress,
+                    soilFertility: [tempProjectDataResult[index].soilFertility]
+                }
+                rawTempProjectData.push(unAvgData);
+                currentRawTempProjetIndex++;
+            }
+        }
+        for (let index = 0; index < rawTempGreenHouseData.length; index++) {
+            let temp = rawTempGreenHouseData[index];
+            temp.temperature = simpleStats.mean(temp.temperature);
+            temp.humidity = simpleStats.mean(temp.humidity);
+            temp.soilMoisture = simpleStats.mean(temp.soilMoisture);
+            temp.ambientLight = simpleStats.mean(temp.ambientLight);
+            rawTempGreenHouseData[index] = temp;
+        }
+        for (let index = 0; index < rawTempProjectData.length; index++) {
+            let temp = rawTempProjectData[index];
+            temp.soilFertility = simpleStats.mean(temp.soilFertility);
+            rawTempProjectData[index] = temp;
         }
     }
 }
@@ -65,10 +138,14 @@ async function getTempProjectData(piMacAddress) {
     })
 }
 
-async function summaryTempGreenHouseData(dataArray) {
+function sendSummarizeData(dataArray, type) {
+    request.post({
+        url: "https://hello-api.careerity.me",
+        body: dataArray,
+        json: true
+    }, {
+        timeout: 20000
+    }, function (err, resopne, body) {
 
-}
-
-async function summaryTempProjectData(dataArray) {
-
+    });
 }
