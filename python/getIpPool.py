67eaaeefc3361d7c39c786224aba4e060ef2466f
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-10-28 21:31:07 +0700

    [update] change try-except

--- python/getIpPool.py
+++ python/getIpPool.py
@@ -5,15 +5,17 @@ from uuid import getnode as get_mac
 def get_mac():
     mac_num = hex(uuid.getnode()).replace('0x', '').upper()
     mac = ':'.join(mac_num[i : i + 2] for i in range(0, 11, 2))
-    macAddress = mac
-    with open("/var/lib/misc/dnsmasq.leases", "r") as ipPollFile:
-        ipPollData = ipPollFile.readlines()
-    print(ipPollData)
-    r = requests.post("https://orchidcareapidev.careerity.me/dynamicControllerHandle", data={'ipPoolData': ipPollData, 'piMacAddress': macAddress}, timeout=20)
+    return mac
 while True:
     try:
-        get_mac()
+        macaddress = get_mac()
+        with open("/var/lib/misc/dnsmasq.leases", "r") as ipPollFile:
+            ipPollData = ipPollFile.readlines()
+        print(ipPollData)
+        r = requests.post("https://orchidcareapidev.careerity.me/dynamicControllerHandle", data={'ipPoolData': ipPollData, 'piMacAddress': macAddress}, timeout=20)
     except requests.exceptions.ConnectionError:
         print("Connection error occur retry...")
+        r.connection.close()
+        time.sleep(30)
         get_mac()
     time.sleep(20)
